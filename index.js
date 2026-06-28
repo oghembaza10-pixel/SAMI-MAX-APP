df38c42a6b358dfa229f850e5a99995d729f747d
oghembaza10-pixel <oghembaza10@gmail.com>
2026-06-28 04:16:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 const express = require("express");
 const axios = require("axios");
-
+const dashboard = require("./routes/dashboard");
 const CONFIG = require("./config");
 const webhook = require("./routes/webhook");
 
