cc0723d14bccb8170e2c1085a635b30c510fd42e
oghembaza10-pixel <oghembaza10@gmail.com>
2026-06-29 04:29:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const academy = require("./routes/academy");
 const app = express();
 const community = require("./routes/community");
 const marketplace = require("./routes/marketplace");
+const drivers = require("./routes/drivers");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -186,6 +187,7 @@ app.use("/login", login);
 app.use("/register", register);
 app.use("/settings", settings);
 app.use("/marketplace", marketplace);
+app.use("/drivers", drivers);
 // ======================================================
 // CHAT SAMII V1
 // ======================================================
