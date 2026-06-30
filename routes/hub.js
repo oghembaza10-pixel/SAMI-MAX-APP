318a117d28cabe53f699b27a19091da7f39ae9f9
oghembaza10-pixel <oghembaza10@gmail.com>
2026-06-30 06:23:53 +0200

    Update hub.js

--- routes/hub.js
+++ routes/hub.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Hub OG en construction");
+router.get('/', (req, res) => {
+    res.render('hub');
 });
 
 module.exports = router;
