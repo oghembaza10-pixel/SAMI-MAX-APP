09bdb0082b267dc8be2d79d05b42f80cec4aa6c5
oghembaza10-pixel <oghembaza10@gmail.com>
2026-07-07 18:49:30 +0200

    Update client.js

--- services/Airtable/client.js
+++ services/Airtable/client.js
@@ -1,3 +1,36 @@
-const airtable = require("../../airtable");
+const airtable = require("./client");
+const config = require("../../config");
 
-module.exports = airtable;
+async function create(data) {
+
+    return airtable.createRecord(
+        config.AIRTABLE.TABLES.CLIENTS,
+        data
+    );
+
+}
+
+async function update(id, data) {
+
+    return airtable.updateRecord(
+        config.AIRTABLE.TABLES.CLIENTS,
+        id,
+        data
+    );
+
+}
+
+async function remove(id) {
+
+    return airtable.deleteRecord(
+        config.AIRTABLE.TABLES.CLIENTS,
+        id
+    );
+
+}
+
+module.exports = {
+    create,
+    update,
+    remove
+};
