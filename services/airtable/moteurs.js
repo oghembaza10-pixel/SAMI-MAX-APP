5374177b0c56c7a7ce0d9fb0c4eb63b6c0041a4b
oghembaza10-pixel <oghembaza10@gmail.com>
2026-07-07 19:11:38 +0200

    Update moteurs.js

--- services/airtable/moteurs.js
+++ services/airtable/moteurs.js
@@ -1,1 +1,16 @@
+const airtable = require("./client");
+const config = require("../../config");
 
+async function create(data) {
+    return airtable.createRecord(config.AIRTABLE.TABLES.MOTEURS, data);
+}
+
+async function update(id, data) {
+    return airtable.updateRecord(config.AIRTABLE.TABLES.MOTEURS, id, data);
+}
+
+async function remove(id) {
+    return airtable.deleteRecord(config.AIRTABLE.TABLES.MOTEURS, id);
+}
+
+module.exports = { create, update, remove };
