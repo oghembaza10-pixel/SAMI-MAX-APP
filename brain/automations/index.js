c3f7351970a9827cf50cacbd2262e9118fbde22d
oghembaza10-pixel <oghembaza10@gmail.com>
2026-07-14 02:16:46 +0200

    Create index.js

---
+++ brain/automations/index.js
@@ -0,0 +1,8 @@
+// brain/automations/index.js
+module.exports = {
+    ...require("./shop"),
+    ...require("./orders"),
+    ...require("./stock"),
+    ...require("./cards"),
+    ...require("./abonnement"),
+};
