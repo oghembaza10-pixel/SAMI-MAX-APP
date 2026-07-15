17119c6456f1d218fb419fa28d3bb028ecc4a9f1
oghembaza10-pixel <oghembaza10@gmail.com>
2026-07-15 03:17:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
 
     PORT: process.env.PORT || 10000,
 
-    APP_URL: process.env.APP_URL || "https://samii.souverain-store.com"
+    APP_URL: process.env.APP_URL || "https://samii.souverain-store.com",
 
     // ==================================================
     // AIRTABLE
