e9ba5a0dede99379f92ee9d352ae177ffe7f1411
OG_EMPIRE _SAMII <oghembaza10@gmail.com>
2026-07-21 13:24:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.use("/drivers",     require("./routes/drivers"));
 app.use("/login",       require("./routes/login"));
 app.use("/register",    require("./routes/register"));
 app.use("/api",         require("./routes/api"));
-
+app.use("/inscription", require("./routes/inscription"));
 // ── PAGE ACCUEIL ──────────────────────────────────────
 app.get("/", (req, res) => res.render("index"));
 
