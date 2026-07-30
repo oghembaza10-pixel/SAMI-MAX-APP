db1fd5cab1c252644d93edc67582035f58e8c110
OG_EMPIRE _SAMII <oghembaza10@gmail.com>
2026-07-30 23:12:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ app.use("/samii/oeil-concurrentiel", require("./routes/oeilconcurrentiel"));
 app.use("/samii/chasseur-stock", require("./routes/chasseurstock"));
 app.use("/samii/memoire-client", require("./routes/memoireclient"));
 app.use("/automatisations", requireAuth, require("./routes/automatisations"));
+app.use("/missions", requireAuth, require("./routes/missions"));
 app.use("/samii",     requireAuth, require("./routes/samii-mode"));
 app.use("/connect",   require("./routes/connector"));
 
